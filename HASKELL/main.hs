0c563cd9d51a292a2e0c70e5449829876ca0cbc8
Subeen Regmi <subeenregmi123@gmail.com>
2023-10-09 17:57:35 +0100

    working on haskell

--- HASKELL/main.hs
+++ HASKELL/main.hs
@@ -7,5 +7,17 @@ doubleNum x = x*2
 
 -- To load this function to ghci use :l filename.hs
 
-canDrink :: Integer -> Boo
+canDrink :: Integer -> Bool
 canDrink x = if x >= 18 then True else False
+
+sayNum :: Int->String
+sayNum x
+    | 1<=x && x <= 5 = show x ++ " " ++ "chosen!"
+    | otherwise      = "Not in between 1 and 5" 
+
+factorec :: (Integral a) => a -> a
+factorec n
+    | n == 0    = 1
+    | otherwise = n * factorec (n-1) 
+
+
