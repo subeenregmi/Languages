fb80091771a8cd58cf438552a7d7ca04036233a3
subeenregmi <subeenregmi123@gmail.com>
2023-10-13 17:17:26 +0100

    more work and notes on haskell

--- HASKELL/main.hs
+++ HASKELL/main.hs
@@ -12,8 +12,10 @@ canDrink x = if x >= 18 then True else False
 
 sayNum :: Int->String
 sayNum x
-    | 1<=x && x <= 5 = show x ++ " " ++ "chosen!"
-    | otherwise      = "Not in between 1 and 5" 
+    | lowerL<=x && x <= upperL = show x ++ " " ++ "chosen!"
+    | otherwise      = "Not in between " ++ show lowerL ++ " and " ++ show upperL 
+    where upperL = 2
+          lowerL = 1
 
 factorec :: (Integral a) => a -> a
 factorec n
@@ -27,3 +29,18 @@ revS (x:xs) = revS (xs) ++ [x]
 len :: (Num a) => [b] -> a 
 len []     = 0 
 len (_:xs) = 1 + len(xs)
+
+sayFive :: (Integral a) => a -> String
+sayFive x
+    | x == 5 = "Five"
+    | x == 4 || x == 3 = "Close.."
+    | otherwise = "Not close"
+
+findFib :: Integer -> Integer
+findFib n = case n of 0 -> 0
+                      1 -> 1
+                      n -> findFib (n-1) + findFib (n-2)
+               
+fibNums :: [Integer]
+fibNums = [findFib x | x <- [0..]]
+ 
